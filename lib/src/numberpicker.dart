9d07cb01352232491d07ab4a066ff581b36feea4
Patrick Reichherzer <patrick.reichherzer@rub.de>
2024-03-02 21:06:48 +0100

    further migration

--- lib/src/numberpicker.dart
+++ lib/src/numberpicker.dart
@@ -6,55 +6,21 @@ import 'package:infinite_listview/infinite_listview.dart';
 typedef TextMapper = String Function(String numberText);
 
 class NumberPicker extends StatefulWidget {
-  /// Min value user can pick
   final int minValue;
-
-  /// Max value user can pick
   final int maxValue;
-
-  /// Currently selected value
   final int value;
-
-  /// Called when selected value changes
   final ValueChanged<int> onChanged;
-
-  /// Specifies how many items should be shown - defaults to 3
   final int itemCount;
-
-  /// Step between elements. Only for integer datePicker
-  /// Examples:
-  /// if step is 100 the following elements may be 100, 200, 300...
-  /// if min=0, max=6, step=3, then items will be 0, 3 and 6
-  /// if min=0, max=5, step=3, then items will be 0 and 3.
   final int step;
-
-  /// height of single item in pixels
   final double itemHeight;
-
-  /// width of single item in pixels
   final double itemWidth;
-
-  /// Direction of scrolling
   final Axis axis;
-
-  /// Style of non-selected numbers. If null, it uses Theme's bodyText2
   final TextStyle? textStyle;
-
-  /// Style of selected numbers. If null, it defaults to Theme's headline5 with a secondary color.
-  final TextStyle? selectedTextStyle;
-
-  /// Whether to trigger haptic pulses or not
+  final TextStyle? selectedTextStyle; // Correctly declare without initialization
   final bool haptics;
-
-  /// Build the text of each item on the picker
   final TextMapper? textMapper;
-
-  /// Pads displayed integer values up to the length of maxValue
   final bool zeroPad;
-
-  /// Decoration to apply to central box where the selected value is placed
   final Decoration? decoration;
-
   final bool infiniteLoop;
 
   const NumberPicker({
@@ -69,7 +35,7 @@ class NumberPicker extends StatefulWidget {
     this.itemWidth = 100,
     this.axis = Axis.vertical,
     this.textStyle,
-    this.selectedTextStyle,
+    this.selectedTextStyle, // Pass through constructor
     this.haptics = false,
     this.decoration,
     this.zeroPad = false,
@@ -89,26 +55,15 @@ class _NumberPickerState extends State<NumberPicker> {
   @override
   void initState() {
     super.initState();
-    final initialOffset =
-        (widget.value - widget.minValue) ~/ widget.step * itemExtent;
-    if (widget.infiniteLoop) {
-      _scrollController =
-          InfiniteScrollController(initialScrollOffset: initialOffset);
-    } else {
-      _scrollController = ScrollController(initialScrollOffset: initialOffset);
-    }
+    _scrollController = widget.infiniteLoop
+        ? InfiniteScrollController(initialScrollOffset: (widget.value - widget.minValue) ~/ widget.step * itemExtent)
+        : ScrollController(initialScrollOffset: (widget.value - widget.minValue) ~/ widget.step * itemExtent);
     _scrollController.addListener(_scrollListener);
   }
 
   void _scrollListener() {
-    var indexOfMiddleElement = (_scrollController.offset / itemExtent).round();
-    if (widget.infiniteLoop) {
-      indexOfMiddleElement %= itemCount;
-    } else {
-      indexOfMiddleElement = indexOfMiddleElement.clamp(0, itemCount - 1);
-    }
-    final intValueInTheMiddle =
-        _intValueFromIndex(indexOfMiddleElement + additionalItemsOnEachSide);
+    final indexOfMiddleElement = (_scrollController.offset / itemExtent).round() % itemCount;
+    final intValueInTheMiddle = _intValueFromIndex(indexOfMiddleElement + additionalItemsOnEachSide);
 
     if (widget.value != intValueInTheMiddle) {
       widget.onChanged(intValueInTheMiddle);
@@ -116,10 +71,6 @@ class _NumberPickerState extends State<NumberPicker> {
         HapticFeedback.selectionClick();
       }
     }
-    Future.delayed(
-      Duration(milliseconds: 100),
-      () => _maybeCenterValue(),
-    );
   }
 
   @override
@@ -137,51 +88,37 @@ class _NumberPickerState extends State<NumberPicker> {
   }
 
   bool get isScrolling => _scrollController.position.isScrollingNotifier.value;
-
-  double get itemExtent =>
-      widget.axis == Axis.vertical ? widget.itemHeight : widget.itemWidth;
-
+  double get itemExtent => widget.axis == Axis.vertical ? widget.itemHeight : widget.itemWidth;
   int get itemCount => (widget.maxValue - widget.minValue) ~/ widget.step + 1;
-
   int get listItemsCount => itemCount + 2 * additionalItemsOnEachSide;
-
   int get additionalItemsOnEachSide => (widget.itemCount - 1) ~/ 2;
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    // Determine the selectedTextStyle here within the build method where themeData is accessible
+    final TextStyle? selectedTextStyle = widget.selectedTextStyle ?? themeData.textTheme.headline5?.copyWith(color: themeData.colorScheme.secondary);
+
     return SizedBox(
-      width: widget.axis == Axis.vertical
-          ? widget.itemWidth
-          : widget.itemCount * widget.itemWidth,
-      height: widget.axis == Axis.vertical
-          ? widget.itemCount * widget.itemHeight
-          : widget.itemHeight,
+      width: widget.axis == Axis.vertical ? widget.itemWidth : widget.itemCount * widget.itemWidth,
+      height: widget.axis == Axis.vertical ? widget.itemCount * widget.itemHeight : widget.itemHeight,
       child: NotificationListener<ScrollEndNotification>(
-        onNotification: (not) {
-          if (not.dragDetails?.primaryVelocity == 0) {
+        onNotification: (notification) {
+          if (notification.dragDetails?.primaryVelocity == 0) {
             Future.microtask(() => _maybeCenterValue());
           }
           return true;
         },
         child: Stack(
           children: [
-            if (widget.infiniteLoop)
-              InfiniteListView.builder(
-                scrollDirection: widget.axis,
-                controller: _scrollController as InfiniteScrollController,
-                itemExtent: itemExtent,
-                itemBuilder: _itemBuilder,
-                padding: EdgeInsets.zero,
-              )
-            else
-              ListView.builder(
-                itemCount: listItemsCount,
-                scrollDirection: widget.axis,
-                controller: _scrollController,
-                itemExtent: itemExtent,
-                itemBuilder: _itemBuilder,
-                padding: EdgeInsets.zero,
-              ),
+            ListView.builder(
+              itemCount: widget.infiniteLoop ? null : listItemsCount,
+              scrollDirection: widget.axis,
+              controller: _scrollController,
+              itemExtent: itemExtent,
+              itemBuilder: (context, index) => _itemBuilder(context, index, selectedTextStyle),
+              padding: EdgeInsets.zero,
+            ),
             _NumberPickerSelectedItemDecoration(
               axis: widget.axis,
               itemExtent: itemExtent,
